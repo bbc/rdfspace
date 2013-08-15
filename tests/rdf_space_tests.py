69ea4d96cc7b59d1d5897329604de30206fda87c
Yves Raimond <yves.raimond@bbc.co.uk>
2013-08-15 10:28:07 +0100

    Adding license header

--- tests/rdf_space_tests.py
+++ tests/rdf_space_tests.py
@@ -1,3 +1,19 @@
+# rdfspace
+# 
+# Copyright (c) 2013 British Broadcasting Corporation
+# 
+# Licensed under the GNU Affero General Public License version 3 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.gnu.org/licenses/agpl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from nose.tools import *
 import numpy as np
 from numpy.linalg import *
