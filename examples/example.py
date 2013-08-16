27de76d513ca9124e586f5ff247c7da20b5e844f
Yves Raimond <yves.raimond@bbc.co.uk>
2013-08-16 10:57:14 +0100

    s/Distance/Similarity

--- examples/example.py
+++ examples/example.py
@@ -3,8 +3,8 @@ from rdfspace.space import Space
 
 space = Space('influencedby.nt', ignored_predicates=['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'], rank=50)
 
-print "Distance betwen JavaScript and ECMAScript:"
+print "Similarity betwen JavaScript and ECMAScript:"
 print space.similarity('http://dbpedia.org/resource/JavaScript', 'http://dbpedia.org/resource/ECMAScript')
 
-print "Distance between Albert Camus and JavaScript:"
+print "Similarity between Albert Camus and JavaScript:"
 print space.similarity('http://dbpedia.org/resource/Albert_Camus', 'http://dbpedia.org/resource/JavaScript')
