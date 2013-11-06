434492f69324136537b8dafaf5bf7dc5e5ba7edc
Yves Raimond <yves.raimond@bbc.co.uk>
2013-11-06 16:33:20 +0000

    Adding centrality to examples

--- README.md
+++ README.md
@@ -30,7 +30,9 @@ Example use
     >>> from rdfspace.space import Space
     >>> space = Space('influencedby.nt', ignored_predicates=['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'], rank=50)
     >>> space.similarity('http://dbpedia.org/resource/JavaScript', 'http://dbpedia.org/resource/ECMAScript')
+    >>> space.centrality('http://dbpedia.org/resource/JavaScript')
     >>> space.similarity('http://dbpedia.org/resource/Albert_Camus', 'http://dbpedia.org/resource/JavaScript')
+    >>> space.centrality('http://dbpedia.org/resource/Albert_Camus')
 
 How it works
 ------------
--- examples/example.py
+++ examples/example.py
@@ -6,5 +6,11 @@ space = Space('influencedby.nt', ignored_predicates=['http://www.w3.org/1999/02/
 print "Similarity betwen JavaScript and ECMAScript:"
 print space.similarity('http://dbpedia.org/resource/JavaScript', 'http://dbpedia.org/resource/ECMAScript')
 
+print "Eigenvector centrality of JavaScript:"
+print space.centrality('http://dbpedia.org/resource/JavaScript')
+
 print "Similarity between Albert Camus and JavaScript:"
 print space.similarity('http://dbpedia.org/resource/Albert_Camus', 'http://dbpedia.org/resource/JavaScript')
+
+print "Eigenvector centrality of Albert Camus"
+print space.centrality('http://dbpedia.org/resource/Albert_Camus')
